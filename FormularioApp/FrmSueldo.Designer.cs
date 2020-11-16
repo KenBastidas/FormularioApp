f2ce7b0d05ff8963e3cc1d18f1fcc34a90528cdf
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2020-11-16 16:11:20 -0500

    Diseño de IU-codificacion y acceso a controles

--- FormularioApp/FrmSueldo.Designer.cs
+++ FormularioApp/FrmSueldo.Designer.cs
@@ -28,12 +28,304 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtHorasTrab = new System.Windows.Forms.TextBox();
+            this.txtValorHora = new System.Windows.Forms.TextBox();
+            this.txtBono = new System.Windows.Forms.TextBox();
+            this.txtTotalIng = new System.Windows.Forms.TextBox();
+            this.txtAsoTrab = new System.Windows.Forms.TextBox();
+            this.txtBar = new System.Windows.Forms.TextBox();
+            this.txtCuentaxPagar = new System.Windows.Forms.TextBox();
+            this.txtTotalEgresos = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sueldo del trabajador";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTotalIng);
+            this.groupBox1.Controls.Add(this.txtBono);
+            this.groupBox1.Controls.Add(this.txtValorHora);
+            this.groupBox1.Controls.Add(this.txtHorasTrab);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(35, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 195);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTotalEgresos);
+            this.groupBox2.Controls.Add(this.txtCuentaxPagar);
+            this.groupBox2.Controls.Add(this.txtBar);
+            this.groupBox2.Controls.Add(this.txtAsoTrab);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(384, 104);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(323, 190);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Egresos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Horas trabajadas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Valor por hora";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Bono de responsabilidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "TOTAL DE INGRESOS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Aso. de Trabajadores";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Bar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 102);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Cuenta por pagar";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 144);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 13);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "TOTAL DE EGRESOS";
+            // 
+            // txtHorasTrab
+            // 
+            this.txtHorasTrab.Location = new System.Drawing.Point(147, 33);
+            this.txtHorasTrab.Name = "txtHorasTrab";
+            this.txtHorasTrab.Size = new System.Drawing.Size(100, 20);
+            this.txtHorasTrab.TabIndex = 4;
+            // 
+            // txtValorHora
+            // 
+            this.txtValorHora.Location = new System.Drawing.Point(147, 77);
+            this.txtValorHora.Name = "txtValorHora";
+            this.txtValorHora.Size = new System.Drawing.Size(100, 20);
+            this.txtValorHora.TabIndex = 5;
+            // 
+            // txtBono
+            // 
+            this.txtBono.Location = new System.Drawing.Point(147, 111);
+            this.txtBono.Name = "txtBono";
+            this.txtBono.Size = new System.Drawing.Size(100, 20);
+            this.txtBono.TabIndex = 6;
+            // 
+            // txtTotalIng
+            // 
+            this.txtTotalIng.Location = new System.Drawing.Point(147, 145);
+            this.txtTotalIng.Name = "txtTotalIng";
+            this.txtTotalIng.ReadOnly = true;
+            this.txtTotalIng.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalIng.TabIndex = 7;
+            // 
+            // txtAsoTrab
+            // 
+            this.txtAsoTrab.Location = new System.Drawing.Point(137, 27);
+            this.txtAsoTrab.Name = "txtAsoTrab";
+            this.txtAsoTrab.Size = new System.Drawing.Size(100, 20);
+            this.txtAsoTrab.TabIndex = 4;
+            // 
+            // txtBar
+            // 
+            this.txtBar.Location = new System.Drawing.Point(137, 65);
+            this.txtBar.Name = "txtBar";
+            this.txtBar.Size = new System.Drawing.Size(100, 20);
+            this.txtBar.TabIndex = 5;
+            // 
+            // txtCuentaxPagar
+            // 
+            this.txtCuentaxPagar.Location = new System.Drawing.Point(137, 102);
+            this.txtCuentaxPagar.Name = "txtCuentaxPagar";
+            this.txtCuentaxPagar.Size = new System.Drawing.Size(100, 20);
+            this.txtCuentaxPagar.TabIndex = 6;
+            // 
+            // txtTotalEgresos
+            // 
+            this.txtTotalEgresos.Location = new System.Drawing.Point(137, 144);
+            this.txtTotalEgresos.Name = "txtTotalEgresos";
+            this.txtTotalEgresos.ReadOnly = true;
+            this.txtTotalEgresos.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalEgresos.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(39, 58);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(115, 13);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Nombre del Trabajador";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(182, 55);
+            this.txtNombre.MaxLength = 50;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(185, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(39, 307);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(81, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Liquido a recibir";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(241, 359);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(89, 40);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(394, 359);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(77, 39);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // FrmSueldo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmSueldo";
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmSueldo";
+            this.Text = "Calculo del sueldo de un trabajador";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTotalIng;
+        private System.Windows.Forms.TextBox txtBono;
+        private System.Windows.Forms.TextBox txtValorHora;
+        private System.Windows.Forms.TextBox txtHorasTrab;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTotalEgresos;
+        private System.Windows.Forms.TextBox txtCuentaxPagar;
+        private System.Windows.Forms.TextBox txtBar;
+        private System.Windows.Forms.TextBox txtAsoTrab;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
--- FormularioApp/FrmSueldo.cs
+++ FormularioApp/FrmSueldo.cs
@@ -16,5 +16,52 @@ namespace FormularioApp
         {
             InitializeComponent();
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Validaciones
+            if(this.txtNombre.Text.Length == 0)
+            {
+                MessageBox.Show("Por favor debes ingresar el nombre...");
+                this.txtNombre.Focus(); //ubica el cursor en el control
+                return ; //abandonar
+            }
+
+            //Tarea:
+            //1.Validar el ingreso de todos los cuadros de textos
+            //2.validar que el dato ingresado sea numerico
+            //no usar try...catch
+            
+
+            //sumar los ingresos
+            double horasTrab = double.Parse(this.txtHorasTrab.Text);
+            double valorHora = double.Parse(this.txtValorHora.Text);
+            double bono = double.Parse(this.txtBono.Text);
+            double totIng = horasTrab * valorHora + bono;
+
+            //mostrar el totIng ewn el cuadro de texto
+            this.txtTotalIng.Text = totIng.ToString();
+
+            //suma los egresos
+            double asoTrab = double.Parse(this.txtAsoTrab.Text);
+            double bar = double.Parse(this.txtBar.Text);
+            double cuentaxPagar = double.Parse(this.txtCuentaxPagar.Text);
+            double totEgre = asoTrab + bar + cuentaxPagar;
+            this.txtTotalEgresos.Text = totEgre.ToString();
+            //liquido a  recibir
+            double liquidoRecibir = totIng - totEgre;
+            lblResultado.Text = "Estimado, " + this.txtNombre.Text +
+                ", tu sueldo es: " + liquidoRecibir.ToString();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close(); //cerrar el formulario actual
+        }
     }
 }
