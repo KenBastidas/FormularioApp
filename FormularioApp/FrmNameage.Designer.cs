06844bb7ac69646695a105c703fb2a5313f95197
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2020-11-22 16:26:03 -0500

    Formulario NameAge

---
+++ FormularioApp/FrmNameage.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace FormularioApp
+{
+    partial class FrmNameage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresar edad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(135, 80);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(135, 124);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(266, 96);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(135, 163);
+            this.txtRespuesta.Multiline = true;
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(206, 110);
+            this.txtRespuesta.TabIndex = 5;
+            // 
+            // FrmNameage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 502);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmNameage";
+            this.Text = "Name and edad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtRespuesta;
+    }
+}
---
+++ FormularioApp/FrmNameage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioApp
+{
+    public partial class FrmNameage : Form
+    {
+        public FrmNameage()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(txtEdad.Text, out double SoloNum))
+            {
+                MessageBox.Show("Ingrese valores numéricos en el cuadro edad....", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                this.txtEdad.Clear();
+                this.txtEdad.Focus();
+                return;
+            }
+
+            int edad = int.Parse(txtEdad.Text);
+            
+            if (!(edad >= 15 && edad <= 80))
+            {
+                MessageBox.Show(txtName.Text + ", Usted no se encuentra en el rango comprendido de edades entre 15-80 años", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            if (edad >= 15 && edad <= 17)
+            {
+
+                txtRespuesta.Text = txtName.Text + ", Eres menor de edad";
+                txtRespuesta.Visible = true;
+            }
+
+            if (edad >= 18 && edad <= 25)
+            {
+
+                txtRespuesta.Text = txtName.Text + ", Puedes votar en las elecciones 2021";
+                txtRespuesta.Visible = true;
+            }
+
+            if (edad >= 26 && edad <= 30)
+            {
+
+                txtRespuesta.Text = txtName.Text + ", Ya debes casarte";
+                txtRespuesta.Visible = true;
+            }
+
+            if (edad >= 31 && edad <= 50)
+            {
+
+                txtRespuesta.Text = txtName.Text + ", Trabaja, duerme y disfruta";
+                txtRespuesta.Visible = true;
+            }
+
+            if (edad > 50)
+            {
+
+                txtRespuesta.Text = txtName.Text + ", Ya casi llegas a la tercera edad";
+                txtRespuesta.Visible = true;
+            }
+
+        }//end btnCalcular
+    }
+}
