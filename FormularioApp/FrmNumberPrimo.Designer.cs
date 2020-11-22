916c202ac9987762094c11dbd77fde23abe43af7
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2020-11-22 15:43:57 -0500

    FORMULARIO SACAR PRIMO

---
+++ FormularioApp/FrmNumberPrimo.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace FormularioApp
+{
+    partial class FrmNumberPrimo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumberEn = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Determine si el numero entero es primo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingresar numero Entero";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtNumberEn
+            // 
+            this.txtNumberEn.Location = new System.Drawing.Point(150, 89);
+            this.txtNumberEn.Name = "txtNumberEn";
+            this.txtNumberEn.Size = new System.Drawing.Size(131, 20);
+            this.txtNumberEn.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Resultado:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(77, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "...";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(150, 216);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(131, 32);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // FrmNumberPrimo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 346);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumberEn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmNumberPrimo";
+            this.Text = "NumberPrimo";
+            this.Load += new System.EventHandler(this.FrmNumberPrimo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumberEn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ FormularioApp/FrmNumberPrimo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioApp
+{
+    public partial class FrmNumberPrimo : Form
+    {
+        public FrmNumberPrimo()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmNumberPrimo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(txtNumberEn.Text, out double SoloNum))
+            {
+                MessageBox.Show("Ingrese solo numeros enteros....", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                this.txtNumberEn.Clear();
+                this.txtNumberEn.Focus();
+                return;
+            }
+            int numero = int.Parse(txtNumberEn.Text);
+            Encontrarprimo(numero);
+        }
+        void Encontrarprimo(int numero)
+        {
+            int cont = 0;
+            for (int k = 1; k <= numero; k++)
+            {
+                if (numero % k == 0)
+                {
+                    cont++;
+                }
+            }
+
+            if (cont == 2)
+            {
+                label4.Text = Convert.ToString(numero + " es primo");
+                label4.Visible = true;
+            }
+            else
+            {
+                label4.Text = Convert.ToString(numero + " no es primo");
+                label4.Visible = true;
+            }
+        }
+    }
+}
