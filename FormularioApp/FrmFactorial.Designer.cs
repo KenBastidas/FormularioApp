3cd4a92a5b125ddc083d446b03504db36bf341a5
BRIAN BASTIDAS <brian6bastidaz@gmail.com>
2020-11-25 18:51:06 -0500

    Formulario Facto

---
+++ FormularioApp/FrmFactorial.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FormularioApp
+{
+    partial class FrmFactorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEntero = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtFacto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Factorial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese un entero";
+            // 
+            // txtEntero
+            // 
+            this.txtEntero.Location = new System.Drawing.Point(122, 68);
+            this.txtEntero.Name = "txtEntero";
+            this.txtEntero.Size = new System.Drawing.Size(100, 20);
+            this.txtEntero.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(122, 94);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtFacto
+            // 
+            this.txtFacto.Location = new System.Drawing.Point(122, 123);
+            this.txtFacto.Name = "txtFacto";
+            this.txtFacto.Size = new System.Drawing.Size(100, 20);
+            this.txtFacto.TabIndex = 4;
+            // 
+            // FrmFactorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFacto);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtEntero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFactorial";
+            this.Text = "FrmFactoial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEntero;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtFacto;
+    }
+}
---
+++ FormularioApp/FrmFactorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioApp
+{
+    public partial class FrmFactorial : Form
+    {
+        public FrmFactorial()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int factorial = int.Parse(txtEntero.Text);
+            facto(factorial);
+            txtFacto.Text = facto(factorial).ToString();
+        }
+        public static double facto(int entero)
+        {
+            if (entero == 0 || entero == 1)
+                return 1;
+            return entero * facto(entero - 1);
+        }
+    }
+}
